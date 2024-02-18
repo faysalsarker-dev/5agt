021c593125826ecb803d930ed8c128f6c8a3563f
Faysal-sarker <fridaysharker100@gmail.com>
2024-02-18 17:32:34 +0600

    finish all js function

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const comon = document.getElementsByClassName('comon');
 const list = document.getElementById('dainamicList');
+const input = document.getElementById('inputbox');
+const DiscountPrice = document.getElementById('DiscountPrice')
+
+
 
 let count = 0;
 
@@ -13,6 +17,9 @@ for(const btn of comon){
 
 function setboking(event){
     count++
+    
+        
+   
     event.target.classList.remove('bg-[#F7F8F8]');
     event.target.classList.add('bg-[#1DD100]');
     event.target.classList.add('text-white');
@@ -22,6 +29,7 @@ function setboking(event){
 
 
     total(count)
+
 
 
     const div = document.createElement('div');
@@ -44,17 +52,32 @@ function setboking(event){
     
     list.appendChild(div);
 
-
+   
     
 
     if(count>=4){
+        
         document.getElementById('applyBtn').removeAttribute('disabled')
+     setinnerText('alerMsg','A parson can buy only 4 tickets')
         for(const btn of comon){
-
+            
             btn.removeEventListener('click',setboking)
 
         }
+       
     }
+    console.log(count)
+    const inputNumber = document.getElementById('inputNumber').value;
+
+    if(count>0 && inputNumber){
+        const btn = document.getElementById('nextBtn');
+        btn.removeAttribute('disabled')
+        console.log('okkk')
+
+    }
+
+
+
 
 
 
@@ -71,10 +94,13 @@ function setinnerText(id,value){
     
 }
 
+
+
 function total(count){
     const totalResult = 550 * count
 
     setinnerText('totalPrice', totalResult)
+    setinnerText('Grandtotal',totalResult)
 }
 
 
@@ -92,4 +118,80 @@ function abvailableset(){
     setinnerText('totalset',result)
    
 
+}
+
+
+function grantTotal(){
+    const pMsg = document.getElementById('invalidMsg')
+    const invalidtext = pMsg.innerText
+    const input = inputbox.value
+
+    if(input === 'NEW15'){
+
+
+       const totalMoney = document.getElementById('totalPrice').innerText
+
+    const sum = totalMoney * 0.15
+
+    const fainal = totalMoney - sum 
+
+document.getElementById('DiscountArea').classList.remove('hidden')
+ 
+setinnerText('DiscountPrice',sum)
+
+
+
+    setinnerText('Grandtotal',fainal)
+
+    inputbox.value=''
+    pMsg.classList.remove('text-rose-500')
+    pMsg.classList.add('text-green-500')
+    setinnerText('invalidMsg' , 'You get 15% off')
+    document.getElementById('inputparent').classList.add('hidden')
+
+
+
+    }else if(input === 'Couple20'){
+        const totalMoney = document.getElementById('totalPrice').innerText
+
+        const sum = totalMoney * 0.20
+    
+        const fainal = totalMoney - sum 
+        setinnerText('Grandtotal',fainal)
+        inputbox.value=''
+        pMsg.classList.remove('text-rose-500')
+        pMsg.classList.add('text-green-500')
+        setinnerText('invalidMsg' , 'You get 20% off')
+
+        document.getElementById('DiscountArea').classList.remove('hidden')
+ 
+        setinnerText('DiscountPrice',sum)
+        document.getElementById('inputparent').classList.add('hidden')
+        
+
+    }else{
+        document.getElementById('DiscountArea').classList.add('hidden')
+ 
+        
+        
+        
+  pMsg.classList.remove('text-green-500')
+    pMsg.classList.add('text-rose-500')
+    pMsg.innerText = 'invalid coupon'
+        inputbox.value = ''
+    
+       
+    }
+    
+
+
+
+}
+
+
+function TicketSell() {
+    const page = document.getElementById('TicketCounter');
+    if (page) {
+        page.scrollIntoView({ behavior: 'smooth' });
+    }
 }
